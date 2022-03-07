1d3849fad72a41ae141fff585eaf6c0a881bf96d
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-03-06 19:37:27 -0700

    Update anagram.py

--- Analisis_de_Datos/anagram.py
+++ Analisis_de_Datos/anagram.py
@@ -1,1 +1,39 @@
+def anagrama(ana1 = '', ana2 = ''):
 
+  dic_ana1 = {}
+  dic_ana2 = {}
+
+  ana1 = ana1.lower().replace('á', 'a')
+  ana1 = ana1.lower().replace('é', 'e')
+  ana1 = ana1.lower().replace('í', 'i')
+  ana1 = ana1.lower().replace('ó', 'o')
+  ana1 = ana1.lower().replace('ú', 'u')
+
+  ana2 = ana2.lower().replace('á', 'a')
+  ana2 = ana2.lower().replace('é', 'e')
+  ana2 = ana2.lower().replace('í', 'i')
+  ana2 = ana2.lower().replace('ó', 'o')
+  ana2 = ana2.lower().replace('ú', 'u')
+
+  for i in ana1.lower().replace(' ', ''):
+    if i in dic_ana1:
+      dic_ana1[i] += 1
+    else:
+      dic_ana1[i] = 1
+
+  for i in ana2.lower().replace(' ', ''):
+    if i in dic_ana2:
+      dic_ana2[i] += 1
+    else:
+      dic_ana2[i] = 1
+
+  if dic_ana1 == dic_ana2:
+    return f'{ana1} - {ana2}: Esto es un anagrama', True
+  else:
+    return f'{ana1} - {ana2}: Esto no es un anagrama', False
+
+
+print(anagrama('válóra', 'Álva ro'))
+print(anagrama('Para', 'A rpa'))
+print(anagrama('Jor gé', 'Jorges'))
+print(anagrama('Azúl', 'Zu la'))
